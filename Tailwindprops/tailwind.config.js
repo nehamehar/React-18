99abaf2eb90fb87e181c5bbc1b331c7d0c0dcb31
Neha Mehar <nehamehar31@gmail.com>
2025-01-29 19:05:25 +0530

    tailwind colors

--- Tailwindprops/tailwind.config.js
+++ Tailwindprops/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [ "./index.html",  // for taking all style of postcss in which colors padding including
+    "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
