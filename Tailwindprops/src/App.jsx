fcd21ceaa7ce8c9fad90c45d7f1b9d2537f387bd
Neha Mehar <nehamehar31@gmail.com>
2025-01-31 03:05:28 +0530

    updated props feature

--- Tailwindprops/src/App.jsx
+++ Tailwindprops/src/App.jsx
@@ -2,17 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import "./src/login page.jsx"
+import Card from './assets/project/loginpage' //importing fun from the location
 
+// Myobj={
+//   username : "samyak",
+//   age:24
+// }
 
 function App() {
   const [count, setCount] = useState(14)
-
+  let myobj={
+    username: "neha",
+    age:24
+  }
+  let newarr=[1,2,3,4]
   return (
     <>
-    
+    <h1 className='bg-red-100 top-p-10 top-px'>hello</h1>
+    <Card name = "neha"  btnText = "Click to go"/>  
+    <Card name = "samyak" btnText = "ARE YOU READY"/>   //what we are passing here will show on props
     </>
   )
 }
-<login page/>
+
 export default App
--- Tailwindprops/src/assets/project/loginpage.jsx
+++ Tailwindprops/src/assets/project/loginpage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 
-function card() {
-  const [count, setCount] = useState(14)
+// function Card(props) {
+//   console.log(props.name)
+
+function Card({name = "samyak", btnText}) {
+  console.log(name)
+ //  const [count, setCount] = useState(14)
 
   return (
     <>
@@ -10,9 +14,9 @@ function card() {
   rel="preload"
   as="image"
   href="https://images.pexels.com/photos/1144275/pexels-photo-1144275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
-<div class="min-h-screen bg-[#121212]">
-  <header class="fixed top-0 z-10 mx-auto w-full max-w-full bg-[#121212] p-6 text-white lg:px-10"><h1>DevUI</h1></header>
-  <div class="mx-auto flex w-full items-stretch justify-between gap-10">
+<div className="min-h-screen bg-[#121212]">
+  <header className="fixed top-0 z-10 mx-auto w-full max-w-full bg-[#121212] p-6 text-white lg:px-10"><h1>DevUI</h1></header>
+  <div className="mx-auto flex w-full items-stretch justify-between gap-10">
     <div class="mt-20 flex w-full flex-col items-start justify-start p-6 md:w-1/2 lg:px-10">
       <div class="w-full">
         <h1 class="mb-2 text-5xl font-extrabold text-white">Forgot password?</h1>
@@ -20,16 +24,16 @@ function card() {
       </div>
       <div class="my-14 flex w-full flex-col items-start justify-start gap-4">
         <div class="flex w-full flex-col items-start justify-start gap-2">
-          <label class="text-xs text-slate-200">Email</label>
+          <label class="text-xs text-slate-200">{name}</label>
           <input
             placeholder="Enter an email..."
             autocomplete="false"
-            type="email"
+            type="Email"
             class="w-full border-[1px] border-white bg-black p-4 text-white placeholder:text-gray-500" />
         </div>
         <button
           class="w-full bg-[#ae7aff] p-3 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e]">
-          Start recovery
+          {btnText}
         </button>
         <p class="my-14 text-sm font-light text-white">
           Already registered?
@@ -49,4 +53,4 @@ function card() {
   )
 }
 
-export default App
+export default Card   // exporting fun 
