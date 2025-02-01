8a2ef147f5377be3c5a12b997cdd94e1a8ba3b6f
Neha Mehar <nehamehar31@gmail.com>
2025-02-01 22:53:02 +0530

    project

---
+++ Background color changer project using tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  const [color, setcolor] = useState("red") //as use of state reflect on screen //color is current value of state//setcolor allow you to update the state// as we passesd usestate (0), so the color initial value is 0
+  return (
+    <>
+    <div className="w-f"></div>
+    </>
+  )
+}
+
+export default App
