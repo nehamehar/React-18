9f03786193397bc6a7d515ab3a1db18d35d8eb15
Neha Mehar <nehamehar31@gmail.com>
2025-03-06 00:14:13 +0530

    Context provider

---
+++ mini-pro/src/assets/context/UserContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const userContext = React.createContext()  //method of react .ceatecontext
+
+export default userContext
---
+++ mini-pro/src/assets/context/UserContextProvider.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import userContext from "./UserContext";
+// creating method
+const UserContextProvider = () => {
+
+}
