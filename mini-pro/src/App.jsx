8960663fa294249e4a2b4b9ab4c5de0f4b9db188
Neha Mehar <nehamehar31@gmail.com>
2025-03-09 05:19:40 +0530

    Update the useContext

--- mini-pro/src/App.jsx
+++ mini-pro/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <UserContextProvider>
-      <h1>React with Chai and share is important</h1>
+      <h1>Hello There</h1>
       <Login />
       <Profile />
     </UserContextProvider>
