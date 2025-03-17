577c80ac548e2741f8dd9b74e4bed1300b480b9f
Neha Mehar <nehamehar31@gmail.com>
2025-03-18 02:28:59 +0530

    Adding component to theme

--- 8) Theme-change/src/App.jsx
+++ 8) Theme-change/src/App.jsx
@@ -28,11 +28,11 @@ function App() {
     <div className="flex flex-wrap items-center min-h-screen">
                 <div className="w-full">
                     <div className="flex justify-end w-full max-w-sm mx-auto mb-4">
-                    {/*themeBtn */}  
+                    <Themebtn/> 
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                    {/* */}
+                    <Card/>
                     </div>
                 </div>
             </div>
